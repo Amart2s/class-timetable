b48406b5fae3c0b87ae67c5ac9ac9aea045c1918
Amart2s <djbaker912315@gmail.com>
2025-08-06 10:30:13 +1000

    begin outline for algorithim

--- backend/algo.py
+++ backend/algo.py
@@ -3,5 +3,5 @@ Algo needs to do the following:
 - take in a list with elements as tuples (name of student, student ID, [classes they are enrolled in])
 - create object for each class, subject is subject ID, length is either 1 or 2 depending on class, 
 size is how many students enrolled
-- 
+- pandas array for timetable
 """
