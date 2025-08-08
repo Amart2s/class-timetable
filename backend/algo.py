b00140ff753a206ca058aeebc489bf01c7226df9
Amart2s <djbaker912315@gmail.com>
2025-08-08 15:27:34 +1000

    starting over

--- backend/algo.py
+++ backend/algo.py
@@ -1,20 +1,2 @@
-from constraint import Problem
-import data
+from ortools.sat.python import cp_model
 
-problem = Problem()
-
-problem.addVariables([s for s in data.subjects], data.time_slots)
-
-for student, subjects in data.students.items():
-    for i in range(len(subjects)):
-        for j in range(i + 1, len(subjects)):
-            problem.addConstraint(lambda t1, t2: t1 != t2, (subjects[i], subjects[j]))
-
-solution = problem.getSolution()
-
-if solution:
-    print("Valid timetable:")
-    for subject, time in sorted(solution.items(), key=lambda x: x[1]):
-        print(f"{subject:10} -> {time}, Students: {[s for s in data.students if s in data.subjects[subject]]}")
-else:
-    print('No valid timetable found.')
--- backend/classes.py
+++ backend/classes.py
@@ -1,35 +1,47 @@
-"""
-when creating a subject, want:
-- class ID
-- How many lectures/tutorials/semenars, how long is each
-- size of class for each lec/sem/tut
-- prof of class
-"""
-
 class Subject():
-
-    def __init__(self, subject, ):
+    def __init__(self, subject, teacher):
         self.subject = subject
-    
-    def get_size(self):
-        return self.size
-    
-    def get_length(self):
-        return self.length
-    
-    def get_subject(self):
-        return self.subject
-
-class Block:
-    def __init__(self, day, start, room=None, filled_by=None):
-        self.day = day
-        self.start = start
-        self.filled_by = filled_by
-        self.room = room
+        self.teacher = teacher
     
     def __repr__(self):
-        return f"{self.filled_by}"
+        return f"{self.subject}, {self.teacher}, hours: {self.duration}"
+    
+    def lecture(self, timeslots):
+        self.lec = Lecture(self.subject, timeslots)
 
-class Timetable:
-    def __init__(self):
-        self.blocks = []
+    def tutorial(self, timeslots):
+        self.tut = Tutorial(self.subject, timeslots)
+
+class Lecture():
+    def __init__(self, subject, timeslots):
+        self.subject = subject
+        self.timeslots = timeslots
+
+class Tutorial():
+    def __init__(self, subject, timeslots):
+        self.subject = subject
+        self.timeslots = timeslots
+
+
+class TimeSlot():
+    def __init__(self, day, time):
+        self.day = day
+        self.time = time
+    
+    def __repr__(self):
+        return f"{self.day} at {self.time}"
+
+class Teacher():
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return f"{self.name}"
+    
+class Room():
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+    
+    def __repr__(self):
+        return f"The {self.name} room, fits {self.size}"
