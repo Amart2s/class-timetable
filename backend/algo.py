8fc04f396843730a4d580e5cf348ba5f21ddc50a
Amart2s <djbaker912315@gmail.com>
2025-08-08 19:33:24 +1000

    updates

--- backend/algo.py
+++ backend/algo.py
@@ -2,21 +2,25 @@ from ortools.sat.python import cp_model
 import pandas as pd
 from classes import TimeSlot
 from classes import Room
+from data import subjects, students, teachers, NUM_SLOTS, days, times
 
 rooms = [Room("Lec 1", 60), Room("Lec 2", 60), Room("Tut 1", 25), Room("Tut 2", 20), Room("Tut 3", 20), Room("Tut 4", 15)]
 
-rows = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-columns = ["9:00am", "10:00am", "11:00am", "12:00am", "1:30pm", "2:30pm", "3:30pm", "4:30pm"]
+data = [[None for _ in range(len(times))] for _ in range(len(days))]
 
-data = [[None for _ in range(len(columns))] for _ in range(len(rows))]
+df = pd.DataFrame(data, index=days, columns=times)
 
-df = pd.DataFrame(data, index=rows, columns=columns)
-
-for column in columns:
-    for row in rows:
-        if column != "12:00am":
-            df.loc[row, column] = TimeSlot(row, column, {room: None for room in rooms})
+for time in times:
+    for day in days:
+        if time != "12:00am":
+            df.loc[day, time] = TimeSlot(day, time, {room: None for room in rooms})
 
 df["12:00am"] = "Mass/Lunch"
+
+model = cp_model.CpModel()
+
+subject_vars = {subject: model.NewIntVar(0, NUM_SLOTS - 1, subject) for subject in subjects}
+
+
 
 print(df.T)
